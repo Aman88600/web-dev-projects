75c48f2528284516b4877fe08657cde452bf5c15
Aman88600 <amanbasoya02@gmail.com>
2025-02-16 23:48:57 +0530

    new idesa

--- project/app_1/models.py
+++ project/app_1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Chats(models.Model):
+    # chat_text = models.CharField(max_length=100)
+    client = models.CharField(max_length=30)
+    bussiness = models.CharField(max_length=30)
+    client_chat = models.BooleanField()
+    time_of_chat = models.DateTimeField()
+
+    def __str__(self):
+        return f'client = {self.client} bussiness = {self.bussiness} is_client = {self.client_chat} time = {self.time_of_chat}'
